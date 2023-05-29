94e1fdb29528543bb25fc3dc8cde59ecfcaaeb7f
krasny <lookjota@gmail.com>
2023-05-28 22:30:11 -0300

    Testes e controller de autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from '@/http/controller/authenticate'
 import { register } from './controller/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
