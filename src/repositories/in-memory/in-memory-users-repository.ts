2ebcd1b27e9546b6e966b46a9bdadcf201820bd6
krasny <lookjota@gmail.com>
2023-05-29 15:39:25 -0300

    Caso de uso de check-in - 3 errors in-memory-check-ins-repository.ts/check-in.spec.ts/check-in.ts

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,30 +1,21 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { User, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
 export class InMemoryUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
+
   public items: User[] = []
-
-  async findById(id: string) {
-    const user = this.items.find((item) => item.id === id)
-
-    if (!user) {
-      return null
-    }
-
-    return user
-  }
-
-  async findByEmail(email: string) {
-    const user = this.items.find((item) => item.email === email)
-
-    if (!user) {
-      return null
-    }
-    return user
-  }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
